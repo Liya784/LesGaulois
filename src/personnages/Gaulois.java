d6e5657c96fa6b30c752c6e7761535da62c7c479
Liya Paret <prm5166a@ensfsi.onmicrosoft.com>
2024-09-19 11:51:02 +0200

    Version1

---
+++ src/personnages/Gaulois.java
@@ -0,0 +1,11 @@
+package personnages;
+
+public class Gaulois {
+	public Gaulois(String nom, int force) {
+		this.nom = nom;
+		this.force = force;
+	}
+	private String nom;
+	private int force;
+	private int effetPotion =1;
+}
