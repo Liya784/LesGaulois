6b19d925f3d38bf8ccb7c1c57b2e71a98ab6fb20
Liya784 <liya.paret1@univ-tlse3.com>
2024-09-24 18:34:50 +0200

    Version 2

--- src/personnages/Gaulois.java
+++ src/personnages/Gaulois.java
@@ -1,11 +1,42 @@
 package personnages;
 
 public class Gaulois {
+	private String nom;
+	private int force;
+	private int effetPotion = 1;
+
 	public Gaulois(String nom, int force) {
 		this.nom = nom;
 		this.force = force;
 	}
-	private String nom;
-	private int force;
-	private int effetPotion =1;
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void parler(String texte) {
+		System.out.println(prendreParole() + "« " + texte + "»");
+	}
+
+	private String prendreParole() {
+		return "Le gaulois " + nom + " : ";
+	}
+
+	public void frapper(Romain romain) {
+		System.out.println(nom + " envoie un grand coup dans la mâchoire de " + romain.getNom());
+		romain.recevoirCoup(force / 3);
+	}
+
+	@Override
+	public String toString() {
+		return "Gaulois [nom=" + nom + ", force=" + force + ", effetPotion=" + effetPotion + "]";
+	}
+
+	public static void main(String[] args) {
+		Gaulois asterix = new Gaulois("Astérix",8);
+		System.out.println(asterix.prendreParole());
+		asterix.parler("Je suis Astérix !");
+		Romain minus = new Romain("Minus",2);
+		asterix.frapper(minus);
+	}
 }
--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -1,5 +1,40 @@
 package personnages;
 
 public class Romain {
+	private String nom;
+	private int force;
 
+	public Romain(String nom, int force) {
+		this.nom = nom;
+		this.force = force;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void parler(String texte) {
+		System.out.println(prendreParole() + "« " + texte + "»");
+	}
+
+	private String prendreParole() {
+		return "Le romain " + nom + " : ";
+	}
+
+	public void recevoirCoup(int forceCoup) {
+		force -= forceCoup;
+		if (force > 0) {
+			parler("Aïe");
+		} else {
+			parler("J'abandonne...");
+		}
+	
+	}
+	public static void main(String[] args) {
+		Romain caiminus = new Romain("Caiminus",9);
+		System.out.println(caiminus.prendreParole());
+		caiminus.parler("Je suis romain !");
+		caiminus.recevoirCoup(7);
+		caiminus.recevoirCoup(3);
+	}
 }
