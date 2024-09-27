7dcc7819980ce34cca1bf22bbd509ac17abbf10b
Liya Paret <prm5166a@ensfsi.onmicrosoft.com>
2024-09-27 09:41:46 +0200

    Début TP2

--- src/personnages/Village.java
+++ src/personnages/Village.java
@@ -1,13 +1,37 @@
 package personnages;
 
 public class Village {
+
+	public Village(String nom, Chef chef, Gaulois[] villageois, int nbVillageois,int nbVillageoisMaximum) {
+		this.nom = nom;
+		this.chef = chef;
+		this.villageois = villageois;
+		this.nbVillageois = nbVillageois;
+	}
+
 	private String nom;
+	private Chef chef;
+	private Gaulois villageois[];
+	private int nbVillageois = 0;
 
 	public Village(String nom) {
 		this.nom = nom;
 	}
 
+	public void setChef(Chef chef) {
+		this.chef = chef;
+	}
+
 	public String getNom() {
 		return nom;
 	}
+	
+	private void ajouterHabitant(Gaulois gaulois) {
+		villageois.length();
+		
+	}
+	
+	
+	
+	
 }
